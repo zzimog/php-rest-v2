3cbe9adad806232f2fa7c0a0b84a14448db26b13
zzimog <null>
2026-07-02 00:56:48 +0200

    Added basic config

---
+++ config.sample.php
@@ -0,0 +1,8 @@
+<?php
+
+define("DB_TYPE", "pgsql");
+define("DB_HOST", "aws-0-eu-west-1.pooler.supabase.com");
+define("DB_PORT", 5432);
+define("DB_NAME", "postgres");
+define("DB_USER", "");
+define("DB_PASW", "");
